ca8614ad505cb2530290dc859226619a6753df4f
Andrey Sadman <sadstatue.socnet@gmail.com>
2021-04-19 12:00:24 +0300

    clone Tweater

---
+++ src/main/java/com/example/sweater/controllers/Registration.java
@@ -0,0 +1,47 @@
+package com.example.sweater.controllers;
+
+import com.example.sweater.domain.User;
+import com.example.sweater.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class Registration {
+    @Autowired
+    private UserService  userService;
+
+    @GetMapping("/registration")
+    public String registration(){
+
+        return "registration";
+    }
+
+    @PostMapping("/registration")
+    public String addUser(User user, Model model){
+
+        if(!userService.addUser(user)){
+            model.addAttribute("message", "User exist");
+            return "/registration";
+        }
+
+
+        return "redirect:/login";
+    }
+    @GetMapping("/activate/{code}")
+    public String activate(Model model, @PathVariable String code){
+
+        boolean isActivated=userService.activateUser(code);
+
+        if (isActivated){
+            model.addAttribute("message", "User successfully activate");
+
+        }else {
+            model.addAttribute("message", "Activation code is not found");
+        }
+        return "login";
+    }
+}
